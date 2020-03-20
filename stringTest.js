83c68af806b1741dba7ed0a3be2985168a0b24d5
gwendal-hannecart <gwendal.hannecart@gmail.com>
2020-03-20 17:18:34 +0100

    test

---
+++ stringTest.js
@@ -0,0 +1,6 @@
+function repeatTest(){
+    console.log(repeat("foo", 3))
+    console.log(repeat("it's me Mario ! ", 50))
+    console.log(repeat("5", 5))
+    console.log(repeat("Oui, ça marche ! ", 1))
+}
